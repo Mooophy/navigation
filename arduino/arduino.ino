fe9b72d77f099c4008b633a7c483000becbdd3ab
mooophy <MoophyAlan@gmail.com>
2014-06-09 11:09:48 +1200

    first commit 	modified:   .gitignore 	new file:   arduino/arduino.ino 	new file:   pc_part/main.cpp 	new file:   pc_part/pc_part.pro

---
+++ arduino/arduino.ino
@@ -0,0 +1,63 @@
+
+
+long distance1, time1;
+long distance2, time2;
+long distance3, time3;
+
+void setup()
+{ 
+  
+  pinMode(5, OUTPUT); 
+  pinMode(6, INPUT);
+  
+  pinMode(7, OUTPUT);  
+  pinMode(8, INPUT);
+  
+  pinMode(9, OUTPUT);
+  pinMode(10,INPUT);
+  
+  Serial.begin(9600);
+}
+void loop()
+{
+  
+   //!  sensor 1    
+   digitalWrite(5,LOW);
+   delayMicroseconds(2); 
+   digitalWrite(5,HIGH);
+   delayMicroseconds(5);
+   time1 = pulseIn(6,HIGH);
+   distance1 = time1 * 0.034;
+   
+   //!  sensor 2  
+   digitalWrite(7,LOW);
+   delayMicroseconds(2); 
+   digitalWrite(7,HIGH);
+   delayMicroseconds(5);  
+   time2 = pulseIn(8,HIGH);
+   distance2 = time2 * 0.034;
+
+
+   //!  sensor 3 
+   digitalWrite(9,LOW);
+   delayMicroseconds(2); 
+   digitalWrite(9,HIGH);
+   delayMicroseconds(5);
+   time3 = pulseIn(10,HIGH);
+   distance3 = time3 * 0.034;  
+   
+   
+  
+    Serial.print(distance1 );
+    Serial.print("|");
+    Serial.print(distance2 );    
+    Serial.print("|");
+    Serial.print(distance3 );
+    Serial.print("|");    
+    Serial.println();
+    
+  delay(200);  
+  
+  
+}
+
---
+++ pc_part/main.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+#include <unistd.h>
+
+int main()
+{
+    //! build bluetooth stream.
+    std::string     raw_data;
+    std::fstream    blueTooth("/dev/rfcomm0");
+    std::vector<int> v(3);
+    while(blueTooth >> raw_data)
+    {
+        std::cout << raw_data << std::endl;
+        std::string distance0, distance1, distance2;
+        std::string::iterator input = raw_data.begin();
+
+        while(*input != '|')
+            distance0.push_back(*input++);
+
+        ++input;
+        while(*input != '|')
+            distance1.push_back(*input++);
+
+        ++input;
+        while(*input != '|')
+            distance2.push_back(*input++);
+
+        v[0] = std::stoi(distance0);
+        v[1] = std::stoi(distance1);
+        v[2] = std::stoi(distance2);
+
+
+        for(auto i : v)
+            std::cout << i << std::endl;
+
+
+//        if(v[0] >= v[1] && v[0] >= v[2]){
+//            std::cout << "turn left"        << std::endl;
+//            system("play left.wav");
+//        }
+
+//        if(v[1] >= v[0] && v[1] >= v[2]){
+//            std::cout << "keep straight"        << std::endl;
+//            system("play mid.wav");
+//        }
+
+//        if(v[2] >= v[1] && v[2] >= v[0]){
+//            std::cout << "turn right"        << std::endl;
+//            system("play right.wav");
+//        }
+
+        //sleep(1);
+        //system("play left.wav");
+    }
+
+}
