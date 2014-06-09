412e4f7a53b2e439434aa3d388ac96daeab90311
mooophy <MoophyAlan@gmail.com>
2014-06-09 18:31:59 +1200

    multithreading added 	modified:   pc_part/main.cpp 	pc_part/left.wav 	pc_part/middle.wav 	pc_part/right.wav 	pc_part/wrong.wav

--- pc_part/main.cpp
+++ pc_part/main.cpp
@@ -1,26 +1,46 @@
 #include <iostream>
-#include <fstream>
-#include <vector>
 #include <string>
 #include <unistd.h>
 #include "sample.hpp"
 #include "queue.hpp"
+#include <thread>
+
+namespace navigation {
+void play( std::size_t* direction )
+{
+    while(true)
+    {
+        switch(*direction)
+        {
+        case 0:     system("play wrong.wav");    break;
+        case 1:     system("play left.wav");     break;
+        case 2:     system("play middle.wav");   break;
+        case 3:     system("play right.wav");    break;
+        }
+    }
+}
+}
 
 int main()
 {
-    //! build bluetooth stream.
-    std::string     raw_data;
-    std::fstream    blueTooth("/dev/rfcomm0");
+    std::fstream blueTooth("/dev/rfcomm0");
+    std::size_t direction = 0;
 
-    navigation::Queue data(10);
+    std::thread back_ground(navigation::play, &direction);
+
+    navigation::Queue data(20);
+    std::string raw_data;
     while(blueTooth >> raw_data)
     {
-        std::cout << "raw : " <<raw_data << std::endl;
         navigation::sample smp(raw_data);
-        std::cout << "smp : " << smp.result() << std::endl;
-
         data << smp.result();
 
+        direction = data.suggest();
+
+        std::cout << "\nraw : " <<raw_data << std::endl;
+        std::cout << "smp : " << smp.result() << std::endl;
         std::cout << "direction suggest: " << data.suggest() << std::endl;
     }
+
+    back_ground.join();
 }
